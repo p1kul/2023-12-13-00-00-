0c5f7166ff3416b65ce558a15bf9369a48a45b3a
p1kul <funikova@me.com>
2024-08-20 00:25:41 +0300

    Add files via upload

--- ls.py
+++ ls.py
@@ -10,20 +10,22 @@ class Bank:
         for i in range(100):
             x = randint(50,500)
             self.balance += x
+
             if self.balance >= 500 and self.lock.locked():
                 self.lock.release()
-            print(f'Пополнение:{x}. Баланс:{self.balance}')
+            print(f'\nПополнение:{x}. Баланс:{self.balance}')
             sleep(0.001)
 
     def take(self):
         for i in range(100):
             x = randint(50, 500)
-            print(f'Запрос на {x}')
+
+            print(f'\nЗапрос на {x}')
             if x <= self.balance:
                 self.balance -= x
-                print(f'Снятие: {x}. Баланс: {self.balance}')
+                print(f'\nСнятие: {x}. Баланс: {self.balance}')
             else:
-                print('Запрос отклонён, недостаточно средств')
+                print('\nЗапрос отклонён, недостаточно средств')
                 self.lock.acquire()
             sleep(0.001)
 bk = Bank()
